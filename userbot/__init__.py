692293be3ef885466a22566c67da62f4b439c3d8
cracker001reborn <60980890+cracker001reborn@users.noreply.github.com>
2020-02-12 22:54:25 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -48,7 +48,7 @@ if bool(ENV):
     # Check if the config was edited by using the already used variable.
     # Basically, its the 'virginity check' for the config file ;)
     CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+        "", None)
 
     if CONFIG_CHECK:
         LOGS.info(
@@ -76,11 +76,10 @@ if bool(ENV):
     DB_URI = os.environ.get("DATABASE_URL", None)
 
     # OCR API key
-    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", "1036322")
 
-    # remove.bg API key
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-
+    # remove.bg API key  
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", "e613b838a98cfef35f41926e1893e3e2")
     # Chrome Driver and Headless Google Chrome Binaries
     CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
